f0d0d32757534929d85e581686e55a4b2ea7b3e1
Daniel Estevez <daniel.ef@gmail.com>
2018-07-18 13:07:21 -0500

    Updating test (resta)

--- test/interware/mx/leveldb_micro_test.clj
+++ test/interware/mx/leveldb_micro_test.clj
@@ -1,6 +1,6 @@
 (ns interware.mx.leveldb-micro-test
   (:require [clojure.test :refer [deftest is testing]]
-            [interware.mx.leveldb-micro :refer [suma]]))
+            [interware.mx.leveldb-micro :refer [suma resta]]))
 
 (deftest -suma
   (testing "Probamos funcion suma"
@@ -11,3 +11,13 @@
     (is (= (suma 0 1 2 3 4 5) 15)))
   (testing "Probamos funcion suma con aridad 6 con letras"
     (is (= (suma 0 1 2 3 "A" 5) nil))))
+
+(deftest -resta
+  (testing "Probamos funcion suma"
+    (is (= (resta 0 1) -1)))
+  (testing "Probamos funcion suma con letras"
+    (is (nil? (resta 0 "A"))))
+  (testing "Probamos funcion suma con aridad 6"
+    (is (= (resta 0 1 2 3 4 5) -15)))
+  (testing "Probamos funcion suma con aridad 6 con letras"
+    (is (= (resta 0 1 2 3 "A" 5) nil))))
