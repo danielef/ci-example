d9e4deb263c33323b1b023b07d97459a1fcea08e
Daniel Estevez <daniel.ef@gmail.com>
2018-07-28 19:55:34 -0500

    Adding IW nexus

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject interware.mx/ci-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ci example project"
+  :url "https://github.com/danielef/ci-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cloverage "1.0.11"]])
+  :plugins [[lein-cloverage "1.0.11"]]
+  
+  :repositories [["snapshots" "https://ci.interware.mx/nexus/repository/interware-snapshot"]
+                 ["releases" "https://ci.interware.mx/nexus/repository/interware-releases"]])
