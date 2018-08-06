05c29c66fa9c9b782c078ba26aaac3e29d368c97
Daniel Estevez <daniel.ef@gmail.com>
2018-08-06 11:25:11 -0500

    Ignoring CA in curl

--- bin/publish-distro.sh
+++ bin/publish-distro.sh
@@ -3,8 +3,6 @@
 # Distro Publisher
 #
 
-echo "TOKEN>$1"
-
 export MKD_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && \
 export ROOT_PATH=$(dirname $MKD_PATH) && \
 export PROJECT=$( grep "defproject" $ROOT_PATH/project.clj ) && \
@@ -13,4 +11,4 @@ export SERVICE_VERSION=$( echo $PROJECT | awk '{print(substr($3,2,length($3)-2))
 export DISTRO_DIR=$SERVICE_NAME-$SERVICE_VERSION && \
 export FILE_DISTRO=$DISTRO_DIR.tgz
 
-curl -v -u "$1" --upload-file $ROOT_PATH/$FILE_DISTRO "https://ci.interware.mx/nexus/repository/interware-distro/$FILE_DISTRO"
+curl -k -v -u "$1" --upload-file $ROOT_PATH/$FILE_DISTRO "https://ci.interware.mx/nexus/repository/interware-distro/$FILE_DISTRO"
